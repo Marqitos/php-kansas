5f25951bc5979a6144b63e54c5e7b0c1b1460ba5
Marqitos <marcosarnoso@msn.com>
2016-02-08 01:38:18 +0100

    Cache javascript
    implementamos de forma eficiente el cache en javascript

--- Module/Javascript.php
+++ Module/Javascript.php
@@ -19,22 +19,49 @@ class Kansas_Module_Javascript
   public function build($components, &$md5 = false) {
     global $application;
     $cache = false;
-    //var_dump($cache = $application->hasModule('BackendCache'), $cache->test('js-' . $md5));
-		$md5 = md5(serialize($components));    
-    if($this->getOptions('cache') &&
-       $cache = $application->hasModule('BackendCache')) {
+    $test = FALSE;
+    if($cache = $application->hasModule('BackendCache')) {
       // TODO: Comprobar crc de todos archivos que componen el resultado
-      if($cache->test('js-' . $md5))
+      if($cache->test('js-' . md5(serialize($components)))) {
+        $data = $cache->load('js-' . md5(serialize($components)));
+        $md5 = md5($data);
+        $dataList = unserialize($data);
+        $test = $dataList['packages'] == $this->getOptions('packages');
+        foreach($dataList['files'] as $path => $crc) {
+          if(!is_readable($path) || $crc != hash_file("crc32b", $path)) {
+            $test = false;
+            break;
+          }
+        }
+      }
+      if($test && $cache->test('js-' . $md5))
   			return $cache->load('js-' . $md5);
-    }
+      else {
+        $fileList = $this->getPackager()->components_to_files($components);
+        $dataList = [
+          'packages'  => $this->getOptions('packages'),
+          'files'     => []
+        ];
+        foreach($fileList as $file)
+          $dataList['files'][$this->getPackager()->get_file_path($file)] = hash_file("crc32b", $this->getPackager()->get_file_path($file));          
+        $data = serialize($dataList);
+        $md5 = md5($data);
+        $cache->save($data, 'js-' . md5(serialize($components)), ['javascript', 'js-index']);
+        $jsCode = $this->javascriptFromComponents($components, $this->getOptions('minifier'));        
+        $cache->save($jsCode, 'js-' . $md5, ['javascript', 'js-code']);
+        return $jsCode;
+      }
+    } else
+      return $this->javascriptFromComponents($components, $this->getOptions('minifier'));
+  }
+  
+  public function javascriptFromComponents($components, $minifier = false) {
     $jsCode = $this->getPackager()->build_from_components($components);
-    if($this->getOptions('minifier')) {
+    if($minifier) {
       require_once 'JShrink/Minifier.php';
-      $jsCode = \JShrink\Minifier::minify($jsCode, $this->getOptions('minifier')); 
-    }
-    if($this->getOptions('cache') && $cache)
-			$cache->save($jsCode, 'js-' . $md5);
-    return $jsCode;
+      return \JShrink\Minifier::minify($jsCode, $minifier); 
+    } else 
+      return $jsCode;
   }
   
   public function getVersion() {
