c1c1234ea2d3280d865b3ede5af78771270c758c
Marcos Porto Mariño <marcosarnoso@msn.com>
2024-05-11 16:19:25 +0200

    Optimizamos la escritura de código JavaScript

--- Layout/Javascript.php
+++ Layout/Javascript.php
@@ -11,12 +11,18 @@
 namespace Kansas\Layout;
 
 use System\Collections\KeyNotFoundException;
+use function array_reverse;
+use function call_user_func;
+use function crc32;
+use function is_callable;
 
 abstract class Javascript {
 
   public const SCRIPT = 'script';
   public const DEPENDENCIES = 'dependencies';
+  public const CRC_32 = 'crc';
   protected $scripts = [];
+  public static $hashs = [];
 
   protected function resolveDependencies(array $script, array &$list) : void {
     if (isset($script[self::DEPENDENCIES])) {
@@ -53,15 +59,63 @@ abstract class Javascript {
     }
   }
 
-  public function loadScript(callable $callable) : void {
+  protected function loadScript(callable $callable) : void {
     $name = '';
     if (is_callable($callable, false, $name)) {
       $this->scripts[$name] = call_user_func($callable);
+      if (isset($this->scripts[$name][self::SCRIPT]) &&
+          !isset($this->scripts[$name][self::CRC_32])) {
+        $this->scripts[$name][self::CRC_32] = crc32($this->scripts[$name][self::SCRIPT]);
+      }
     } else {
       require_once 'System/Collections/KeyNotFoundException.php';
       throw new KeyNotFoundException($name);
     }
   }
+
+  public static function write(...$scripts) {
+    $parts = array_merge(...$scripts);
+    $fingerprint = '';
+
+    // Escribimos los scripts
+    foreach ($parts as &$part) {
+      if (!isset($part[self::CRC_32])) {
+        $part[self::CRC_32] = crc32($part[self::SCRIPT]);
+      }
+
+      // Omitimos los que están en la lista
+      if (in_array($part[self::CRC_32], self::$hashs)) {
+        continue;
+      }
+
+      // Creamos la lista
+      $fingerprint .= sprintf('%s08', dechex($part[self::CRC_32]));
+
+      echo $part[self::SCRIPT];
+    }
+
+    // Escribimos el fingerprint
+    ob_start(); ?>
+
+if('scripts' in window) {
+  window.scripts += '<?= $fingerprint ?>';
+} else {
+  window.scripts = '<?= $fingerprint ?>';
+}
+
+    <?php
+    echo ob_get_clean();
+
+  }
+
+  public static function setFingerprint(string $fingerprint) : void {
+    while (strlen($fingerprint) >= 8) {
+      $hash = substr($fingerprint, 0, 8);
+      self::$hashs[] = hexdec($hash);
+      $fingerprint = substr($fingerprint, 8);
+    }
+  }
+
 }
 
 global $javascript;
